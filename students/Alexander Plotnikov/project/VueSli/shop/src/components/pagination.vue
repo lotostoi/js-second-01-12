14d14c1b76083bb16df24ef4c2cf2253d735e83b
lotostoii <lotostoii@gmail.com>
2020-01-16 14:11:59 +1000

    разбираюсь

--- students/Alexander Plotnikov/project/VueSli/shop/src/components/pagination.vue
+++ students/Alexander Plotnikov/project/VueSli/shop/src/components/pagination.vue
@@ -3,44 +3,24 @@
     <resize-observer @notify="handleResize" />
     <ul class="paginationn">
       <li class="page-item">
-        <button
-          class="page-link"
-          href="#"
-          aria-label="Previous"
-          @click="getList(currentIndex - 1)"
-        >
-          <img
-            src="./assets/Next_left.svg"
-            alt="previos"
-            class="page-link__img"
-          />
+        <button class="page-link" href="#" aria-label="Previous" @click="getList(currentIndex - 1)">
+          <img src="./assets/Next_left.svg" alt="previos" class="page-link__img" />
         </button>
       </li>
 
       <li
-        v-on:click="getList(index)"
+        v-on:click="clickPage(item.id)"
         v-for="(item, index) in listStile"
         :key="index"
         :index="index"
         class="page-item"
       >
-        <button class="page-link" :class="{ 'page-link-active': item.active }">
-          {{ index + 1 }}
-        </button>
+        <button class="page-link" :class="{ 'page-link-active': item.active }">{{ item.id}}</button>
       </li>
 
       <li class="page-item">
-        <button
-          class="page-link"
-          href="#"
-          aria-label="Next"
-          @click="getList(currentIndex + 1)"
-        >
-          <img
-            src="./assets/Next_right.svg"
-            alt="previos"
-            class="page-link__img"
-          />
+        <button class="page-link" href="#" aria-label="Next" @click="getList(currentIndex + 1)">
+          <img src="./assets/Next_right.svg" alt="previos" class="page-link__img" />
         </button>
       </li>
     </ul>
@@ -62,7 +42,9 @@ export default {
       quantPage: 10, // чисило страниц
       quantProd: 12, // число продуктов на странице
       currentIndex: 0, // номер текущей страници
-      listStile: [] // массив со свойством для каждого значения пагинации, свойство отвечает за формление кнопки когда она выбрана
+      listStile: [], // массив со свойством для каждого значения пагинации, свойство отвечает за формление кнопки когда она выбрана
+      minPage: 1, // начальное значение в диапазоне вывода страниц в пагинации
+      maxPage: 10 // конечное значение в диапазоне вывода страниц в пагинации
     };
   },
 
@@ -96,18 +78,49 @@ export default {
     creatClassList(min, max) {
       let arr = [];
       for (let i = min; i < max; i++) {
-        arr.push({ active: false });
+        arr.push({ active: false, id: i });
       }
+      window.console.log(arr);
       return arr;
     },
     creatArrCurrentPages(index) {
-      if (this.allQuantPages > this.quantPage) {
-        this.listStile = this.creatClassList(0, this.allQuantPages);
+      if (index < this.maxPage - 2 ) {
+        this.listStile = this.creatClassList(this.minPage, this.maxPage);
+        window.console.log(this.minPage + " " + this.maxPage);
       }
 
-      this.listStile = this.creatClassList(this.allQuantPages);
-      window.console.log(this.listStile);
-      this.listStile[this.currentIndex].active = true;
+      if (index >= this.maxPage - 2) {
+        if (this.maxPage <= this.allQuantPages - 2) {
+          this.minPage += 2;
+          this.maxPage += 2;
+        }
+        if (this.maxPage == this.allQuantPages - 1) {
+          this.minPage += 1;
+          this.maxPage += 1;
+        }
+        this.listStile = this.creatClassList(this.minPage, this.maxPage);
+      }
+
+      if (index <= this.minPage + 2) {
+        if (this.minPage <= 3 && index > 2) {
+          this.minPage -= 2;
+          this.maxPage -= 2;
+         
+        }
+        if (this.minPage == 2  && index > 2) {
+          this.minPage -= 1;
+          this.maxPage -= 1;
+          
+        }
+        this.listStile = this.creatClassList(this.minPage, this.maxPage);
+      }
+      window.console.log(this.minPage + " " + this.maxPage);
+      this.listStile.find(item => item.id == this.currentIndex).active = true;
+    },
+    clickPage(index) {
+      this.reRenderPage(this.getList(index));
+
+      this.creatArrCurrentPages(this.currentIndex);
     }
   },
   mounted: function() {
@@ -115,6 +128,10 @@ export default {
       this.$store.state.catalog = data;
       this.$store.commit("getCatalog", data);
       this.reRenderPage(this.getList(0));
+      this.creatArrCurrentPages(this.currentIndex);
+      this.listStile.forEach(el => {
+        window.console.log(el.id);
+      });
     });
   },
   computed: {
